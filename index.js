366e869abe2013e50c9c3fdb439cb191d209a901
gautami2i <gautam.vijayan@ideas2it.com>
2022-12-14 16:08:36 +0530

    sample

---
+++ index.js
@@ -0,0 +1,43 @@
+// let record = [
+//     {
+//         "name":"vikram",
+//         "age":"12",
+//         "work":"asdsad"
+//     },
+//     {
+//         "name":"vikram",
+//         "age":"12",
+//         "work":"asdsad"
+//     },
+//     {
+//         "name":"vikram",
+//         "age":"12",
+//         "work":"asdsad"
+//     },
+// ]
+
+// tableData(record);
+// debugger;
+
+// const parentDiv = document.getElementById("container");
+//     let table = document.getElementById("table");
+//     var createTable = document.createElement("table");
+//     console.log("----------=====",record);
+// function tableData(th) {
+//     console.log("record",th);
+//    let keys =   Object.keys(th);
+//    console.log("sadsaf-====+++",keys)
+//     for(let x of keys) {
+//       var row = createTable.insertRow();
+//       createTable.appendChild(row);
+//       var cell1 = row.insertCell();
+//       var text = document.createTextNode(x);
+//       cell1.appendChild(text);
+//       parentDiv.appendChild(createTable);
+//     }
+      
+//     }
+
+ function sam() {
+        return "hello";
+    }
